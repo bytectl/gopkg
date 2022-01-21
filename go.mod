834059c8bf924abac5f55b3b31819044f00a9fd1
wuxingzhong <330332812@qq.com>
2022-01-21 17:34:12 +0800

    add middleware

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/bytectl/gopkg
 
 go 1.17
 
-require github.com/gogf/gf v1.16.6 // indirect
+require (
+	github.com/go-kratos/kratos/v2 v2.1.4 // indirect
+	github.com/go-playground/form/v4 v4.2.0 // indirect
+	github.com/gogf/gf v1.16.6 // indirect
+	google.golang.org/genproto v0.0.0-20210805201207-89edb61ffb67 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
---
+++ middleware/auth/uid/user.go
@@ -0,0 +1,47 @@
+package uid
+
+import (
+	"context"
+
+	"github.com/go-kratos/kratos/v2/middleware"
+	"github.com/go-kratos/kratos/v2/transport"
+)
+
+type uidKey struct{}
+
+const (
+	authUserKey string = "X-Auth-User"
+)
+
+type UserData struct {
+	ID string
+}
+
+// Server is a server auth middleware. Get the user data from Header.
+func Server() middleware.Middleware {
+
+	return func(handler middleware.Handler) middleware.Handler {
+		return func(ctx context.Context, req interface{}) (interface{}, error) {
+			if header, ok := transport.FromServerContext(ctx); ok {
+				id := header.RequestHeader().Get(authUserKey)
+				if id != "" {
+					ctx = NewContext(ctx, UserData{
+						ID: id,
+					})
+				}
+			}
+			return handler(ctx, req)
+		}
+	}
+}
+
+// NewContext put user info into context
+func NewContext(ctx context.Context, info UserData) context.Context {
+	return context.WithValue(ctx, uidKey{}, info)
+}
+
+// FromContext extract user info from context
+func FromContext(ctx context.Context) (info UserData, ok bool) {
+	info, ok = ctx.Value(uidKey{}).(UserData)
+	return
+}
