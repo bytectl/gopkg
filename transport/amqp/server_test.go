7913a40ce6f22cd937f7c23ab74c60394329c5f7
wuxingzhong <330338212@qq.com>
2022-05-01 20:56:43 +0800

    fix amqp

--- transport/amqp/server_test.go
+++ transport/amqp/server_test.go
@@ -28,7 +28,7 @@ func TestServer(t *testing.T) {
 			panic(err)
 		}
 	}()
-	time.Sleep(time.Second * 15)
+	time.Sleep(time.Second * 1)
 	if srv.Stop(ctx) != nil {
 		t.Errorf("expected nil got %v", srv.Stop(ctx))
 	}
