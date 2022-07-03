3f3177e0ba938e82190a259c532ca5f75849a6dd
wuxingzhong <330332812@qq.com>
2022-07-03 22:54:44 +0800

    change tab to spaces

--- tsl/codec_template.go
+++ tsl/codec_template.go
@@ -4,24 +4,24 @@ var DefaultCodecTmpl = `// {{ .Profile.ProductKey }} 产品编解码插件
 package codec
 
 import (
-	"bytes"
-	"encoding/binary"
-	"encoding/json"
-	"fmt"
+    "bytes"
+    "encoding/binary"
+    "encoding/json"
+    "fmt"
 )
 
 type Event struct {
-	Params map[string]interface{} CODEBLOCKjson:"params"CODEBLOCK
-	Method string                 CODEBLOCKjson:"method"CODEBLOCK
+    Params map[string]interface{} CODEBLOCKjson:"params"CODEBLOCK
+    Method string                 CODEBLOCKjson:"method"CODEBLOCK
 }
 type EventData struct {
-	Events []Event CODEBLOCKjson:"events"CODEBLOCK
+    Events []Event CODEBLOCKjson:"events"CODEBLOCK
 }
 
 type Params map[string]interface{}
 
 const ({{range $index, $element := .Value.Events }}
-	Event{{ title $element.ConstName }} = "{{$element.Method}}" // {{$element.Name}}事件{{end}} 
+    Event{{ title $element.ConstName }} = "{{$element.Method}}" // {{$element.Name}}事件{{end}} 
 )
 
 {{- range $index, $element := .Value.Events }}
@@ -31,48 +31,48 @@ const ({{range $index, $element := .Value.Events }}
 {{- range $element.OutputData}}func (p Params) Set{{ $prefixName }}{{- title .Identifier }}(v {{.DataType.GenerateGoType -}}) { p["{{- .Identifier -}}"] = v } // {{.Name}}
 {{end -}}{{end -}}
 
-// 解码	
+// 解码    
 func Decode(payload, metadata []byte) ([]byte, error) {
 
-	buffer := bytes.NewBuffer(payload)
-	var decodeData struct {
-		//TODO: please change to you decode fields
-		{{- range .Value.Events}}
+    buffer := bytes.NewBuffer(payload)
+    var decodeData struct {
+    //TODO: please change to you decode fields
+    {{- range .Value.Events}}
 
 
-		{{$prefixName := title .ParamPrefixName}}
-		{{- if not $prefixName }}
-		{{- range .OutputData}}{{- title .Identifier }}  {{.DataType.GenerateGoType}} 
-		{{end -}}{{end -}}{{end -}}
-	}
-	if err := binary.Read(buffer, binary.BigEndian, &decodeData); err != nil {
-		return nil, err
-	}
+    {{$prefixName := title .ParamPrefixName}}
+    {{- if not $prefixName }}
+    {{- range .OutputData}}{{- title .Identifier }}  {{.DataType.GenerateGoType}} 
+    {{end -}}{{end -}}{{end -}}
+    }
+    if err := binary.Read(buffer, binary.BigEndian, &decodeData); err != nil {
+    return nil, err
+    }
 
-	params := make(Params)
-	//TODO: please set your params
-	{{- range .Value.Events}}
-	{{$prefixName := title .ParamPrefixName}}
-	{{- if not $prefixName }}
-	{{- range .OutputData}}params.Set{{- title .Identifier }}({{- title .Identifier }} ) // {{.Name}}
-	{{end -}}{{end -}}{{end -}}
-	//TODO: please make up your other event params
+    params := make(Params)
+    //TODO: please set your params
+    {{- range .Value.Events}}
+    {{$prefixName := title .ParamPrefixName}}
+    {{- if not $prefixName }}
+    {{- range .OutputData}}params.Set{{- title .Identifier }}({{- title .Identifier }} ) // {{.Name}}
+    {{end -}}{{end -}}{{end -}}
+    //TODO: please make up your other event params
 
-	eventData := &EventData{
-		Events: []Event{
-			{
-				Params: params,
-				//TODO: please change to you need event method
-				Method: EventPropertyMethod,
-			},
-		},
-	}
-	return json.Marshal(eventData)
+    eventData := &EventData{
+    Events: []Event{
+    {
+    Params: params,
+    //TODO: please change to you need event method
+    Method: EventPropertyMethod,
+    },
+    },
+    }
+    return json.Marshal(eventData)
 }
 
 // Encode 编码
 func Encode(data, metadata []byte) ([]byte, error) {
-	return nil, fmt.Errorf("not implement")
+    return nil, fmt.Errorf("not implement")
 }
 
 `
